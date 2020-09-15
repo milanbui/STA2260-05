fffb46d49d627043e6e93b2ebb97c7d759aa49ed
Milan B <43893040+milanbui@users.noreply.github.com>
2020-09-15 14:27:36 -0700

    Add files via upload

---
+++ Bui_Milan_tr.R
@@ -0,0 +1,11 @@
+data <- c(1, -3, 5, 7, 4, 11, -13, 2); data # after the semi-colon just prints the contents of the variable
+
+sum(data) # Only the function is being run here, everything after the # sign is a comment and ignored when run! Feel free to place comments like this.
+
+mean(data)
+
+data^2
+
+data + 5
+
+?mean # ? before some functions gives the documentation for it
